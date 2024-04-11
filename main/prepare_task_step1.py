66c6a5af0639ce4e3f3e8c1fd1fed5c8453106c5
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-04-11 11:24:18 +0530

    redirecting result to an output file

--- main/prepare_task_step1.py
+++ main/prepare_task_step1.py
@@ -1,19 +1,21 @@
 import argparse
 import configparser
+import sys
 
-def main(number, other_number):
+def main(number, other_number, output):
     result = number * other_number
-    print(f'the result is {result}')
+    print(f'the result is {result}', file=output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n1', type=int, help='A number', default=1)
     parser.add_argument('-n2', type=int, help='Another number', default=1)
     parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='config file')
+    parser.add_argument('-o', dest='output', type=argparse.FileType('w'), help='output file', default=sys.stdout)
     args = parser.parse_args()
     if args.config:
         config = configparser.ConfigParser()
         config.read_file(args.config)
         args.n1 = int(config['ARGUMENTS']['n1'])
         args.n2 = int(config['ARGUMENTS']['n2'])
-    main(args.n1, args.n2)
+    main(args.n1, args.n2, args.output)
