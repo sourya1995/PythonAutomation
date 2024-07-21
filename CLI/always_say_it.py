3b3ff4d231fad57f2cc78ca60a398e3f22941727
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-21 10:43:15 +0530

    simple CLI tool

---
+++ CLI/always_say_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+
+def say_it():
+    greeting = "Hello"
+    target = "Joe"
+    message = f'{greeting}, {target}!'
+    print(message)
+
+if __name__ == "__main__":
+    say_it()
