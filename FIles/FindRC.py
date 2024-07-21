7710e48dd3c2e6fb4a7ed0a1d7caa8726f9b997d
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-21 10:02:22 +0530

    using PathLib

--- FIles/FindRC.py
+++ FIles/FindRC.py
@@ -1,36 +1,36 @@
+from pathlib import Path
 import os
 
 
 def find_rc(rc_name=".examplerc"):
     var_name = "EXAMPLERC_DIR"
     if var_name in os.environ:
-        var_path = os.path.join(f"{var_name}", rc_name)
-        config_path = os.path.expandvars(var_path)
-        print(f"Checking {config_path}")
-        if os.path.exists(config_path):
-            return config_path
+        var_path = Path(os.environ[var_name]) / rc_name
+        print(f"Checking {var_path}")
+        if var_path.exists():
+            return str(var_path)
     
-    config_path = os.path.join(os.getcwd(), rc_name)
+    config_path = Path.cwd() / rc_name
     print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    if config_path.exists():
+        return str(config_path)
     
-    home_dir = os.path.expanduser("~/")
-    config_path = os.path.join(home_dir, rc_name)
+    home_dir = Path.home()
+    config_path = home_dir / rc_name
     print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    if config_path.exists():
+        return str(config_path)
     
-    file_path = os.path.abspath(__file__)
-    parent_path = os.path.dirname(file_path)
-    config_path = os.path.join(parent_path, rc_name)
+    file_path = Path(__file__).resolve()
+    parent_path = file_path.parent
+    config_path = parent_path / rc_name
     print(f"Checking {config_path}")
-    if os.path.exists(config_path):
-        return config_path
+    if config_path.exists():
+        return str(config_path)
     
     print(f"File {rc_name} not found in any of the following locations:")
     print(f"1. Environment variable {var_name}")
     print(f"2. Current working directory")
     print(f"3. Home directory")
     print(f"4. Parent directory of the current file")
-    
+    raise FileNotFoundError(f"File {rc_name} not found in any of the above locations.")
